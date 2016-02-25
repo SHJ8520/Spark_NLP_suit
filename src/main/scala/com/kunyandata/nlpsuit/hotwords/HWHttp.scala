81e3ce674b5ff72d5ce67c82cec8ac760c8bc850
sunhuijun <sunhuijun@kunyan-inc.com>
2016-02-25 11:50:43 +0800

    算法代码重构

--- src/main/scala/com/kunyandata/nlpsuit/hotwords/HWHttp.scala
+++ src/main/scala/com/kunyandata/nlpsuit/hotwords/HWHttp.scala
@@ -5,7 +5,7 @@ import scala.collection.mutable
 
 /**
   * Created by Administrator on 2016/1/26.
-  */
+   */
 object HWHttp extends BaseHttp{
   override def get(strUrl: String, parameters: mutable.HashMap[String, String], parse: String): Unit = super.get(strUrl, parameters, parse)
   def sendNew(url:String,parameters: mutable.HashMap[String, String]): Unit ={
